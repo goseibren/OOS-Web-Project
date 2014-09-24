0a5d1e61d542057230560215203c5d0dd9d16e02
Bren Yamaguchi <byamaguchi92@gmail.com>
2014-09-24 01:37:32 -0700

    productspage: made only JQUERY; CustomsPage: hover over blue, different Text Logic;

--- WebsiteRoot/js/loadpagecustom.js
+++ WebsiteRoot/js/loadpagecustom.js
@@ -4,10 +4,10 @@ $(function(){
 	var $customdivs = $('#customimages div');
 	var $customimages = $('#customimages div #custom1,#custom2,#custom3');
 
-	$headeroverlay.animate({opacity: 0.3}, 1800, function(){
-		$customdivs.eq(0).animate({opacity: 1}, 700, function(){
-			$customdivs.eq(1).animate({opacity: 1}, 700, function(){
-				$customdivs.eq(2).animate({opacity: 1}, 700, function(){
+	$headeroverlay.animate({opacity: 0.3}, 1250, function(){
+		$customdivs.eq(0).animate({opacity: 1}, 500, function(){
+			$customdivs.eq(1).animate({opacity: 1}, 500, function(){
+				$customdivs.eq(2).animate({opacity: 1}, 500, function(){
 				});
 			});
 		});
@@ -15,12 +15,27 @@ $(function(){
 
 	$customdivs.mouseenter(function(){
 		var $this = $(this);
-		$this.prepend('<div id="overlayservicebox"></div><p class="middletext">Text Will Go Here For Screen Printing</p>');
-		var $overlayservicebox = $('#overlayservicebox');
+		var currentIndex = $this.index();
+
+		switch(currentIndex){
+			case 0:
+				$this.prepend('<p class="middletext">Text Will Go Here For Screen Printing<br>Text Will Go Here For Screen Printing<br>Text Will Go Here For Screen Printing</p>');
+				break;
+			case 1:
+				$this.prepend('<p class="middletext">Text Will Go Here For Heat Transfer<br>Text Will Go Here For Heat Transfer<br>Text Will Go Here For Heat Transfer</p>');	
+				break;
+			case 2:
+				$this.prepend('<p class="middletext">Text Will Go Here For Stickers/Decals<br>Text Will Go Here For Stickers/Decals<br>Text Will Go Here For Stickers/Decals</p>');
+				break;
+		}
+		$this.prepend('<div id="overlayservicebox"></div>');
+
 		var $overlayboxtext = $('#customimages .middletext');
+		var $overlayservicebox = $('#overlayservicebox');
+
 		$overlayservicebox.css({
-			'background-color': 'black',
-			'opacity': '0.7',
+			'background-color': '0868a5',
+			'opacity': '0.85',
 			'width': '100%',
 			'height': '100%',
 			'position': 'absolute',
@@ -37,6 +52,7 @@ $(function(){
 			'top': '25%',
 			'font-size': '22px',
 			'z-index': '100',
+
 		});
 	});
 
--- WebsiteRoot/js/loadpageproduct2.js
+++ WebsiteRoot/js/loadpageproduct2.js
@@ -1,71 +1,100 @@
-var isProdHeader = true;
-var isProducts = true;
+//*******************************//
+//***JAVASCRIPT IMPLEMENTATION***//
+//*******************************//
 
-var prodHeadElem = document.querySelector('.backgroundfixed');
-var prodElem = document.querySelectorAll('.productimages img');
-var prodElemText = document.querySelectorAll('.productimages p');
+// var isProdHeader = true;
+// var isProducts = true;
 
-function animateProdHeader(elem,style,unit,from,to,time) {
-	isAnimating = true;
-    isProdHeader = true;
-    if( !elem) {
-    	advance();
-    	return;
-    }
-    var start = new Date().getTime();
-        var animProdHeaderTimer = setInterval(function() {
-            var step = Math.min(1,(new Date().getTime()-start)/time);
-            elem.style[style] = (from+step*(to-from))+unit;
-            if( step == 1) {
-            	clearInterval(animProdHeaderTimer);
-            	isAnimating = false;
-                isProdHeader = false;
-            }
-        },25);
-    elem.style[style] = from+unit;
-}
+// var prodHeadElem = document.querySelector('.backgroundfixed');
+// var prodElem = document.querySelectorAll('.productimages img');
+// var prodElemText = document.querySelectorAll('.productimages p');
 
-function animateProducts(elem,style,unit,from,to,time) {
-    isAnimating = true;
-    isProducts = true;
-    if( !elem) {
-        advance();
-        return;
-    }
-    var start = new Date().getTime();
-        var productTimer = setInterval(function() {
-            var step = Math.min(1,(new Date().getTime()-start)/time);
-            elem.style[style] = (from+step*(to-from))+unit;
-            if( step == 1) {
-                clearInterval(productTimer);
-                isAnimating = false;
-                isProducts = false;
-            }
-        },25);
-    elem.style[style] = from+unit;
-}
+// function animateProdHeader(elem,style,unit,from,to,time) {
+// 	isAnimating = true;
+//     isProdHeader = true;
+//     if( !elem) {
+//     	advance();
+//     	return;
+//     }
+//     var start = new Date().getTime();
+//         var animProdHeaderTimer = setInterval(function() {
+//             var step = Math.min(1,(new Date().getTime()-start)/time);
+//             elem.style[style] = (from+step*(to-from))+unit;
+//             if( step == 1) {
+//             	clearInterval(animProdHeaderTimer);
+//             	isAnimating = false;
+//                 isProdHeader = false;
+//             }
+//         },25);
+//     elem.style[style] = from+unit;
+// }
 
-animateProdHeader(prodHeadElem, "opacity", "", 0, 1, 1250);
+// function animateProducts(elem,style,unit,from,to,time) {
+//     isAnimating = true;
+//     isProducts = true;
+//     if( !elem) {
+//         advance();
+//         return;
+//     }
+//     var start = new Date().getTime();
+//         var productTimer = setInterval(function() {
+//             var step = Math.min(1,(new Date().getTime()-start)/time);
+//             elem.style[style] = (from+step*(to-from))+unit;
+//             if( step == 1) {
+//                 clearInterval(productTimer);
+//                 isAnimating = false;
+//                 isProducts = false;
+//             }
+//         },25);
+//     elem.style[style] = from+unit;
+// }
 
-var productWaitTimer = setInterval(function(){ //logo fade in.
-    console.log(isProdHeader);
-    if(isProdHeader === false){
-        console.log("hi");
-        for(var i=0; i < prodElem.length; i++){
-            animateProducts(prodElem[i], "opacity", "", 0, 1, 1500);
-            animateProducts(prodElemText[i], "opacity", "", 0, 1, 1500);
-            animateProducts(prodElem[i], "margin-top", "%", -8, 0, 1200);
-            animateProducts(prodElem[i], "top", "%", -8, 0, 1200);
-        }
-        for(var i=0; i < prodElemText.length; i++){
-            animateProducts(prodElemText[i], "opacity", "", 0, 1, 1500);
-        }
-        clearInterval(productWaitTimer);
-    }
-}, 25);
-console.log("hello");
-// for(var i=0; i < prodElem.length; i++)
-//             animateProducts(prodElem[i], "opacity", "", 0, 1, 4000);
+// animateProdHeader(prodHeadElem, "opacity", "", 0, 1, 1250);
+
+// var productWaitTimer = setInterval(function(){ //logo fade in.
+//     console.log(isProdHeader);
+//     if(isProdHeader === false){
+//         console.log("hi");
+//         for(var i=0; i < prodElem.length; i++){
+//             animateProducts(prodElem[i], "opacity", "", 0, 1, 1500);
+//             animateProducts(prodElemText[i], "opacity", "", 0, 1, 1500);
+//             animateProducts(prodElem[i], "margin-top", "%", -8, 0, 1200);
+//             animateProducts(prodElem[i], "top", "%", -8, 0, 1200);
+//         }
+//         for(var i=0; i < prodElemText.length; i++){
+//             animateProducts(prodElemText[i], "opacity", "", 0, 1, 1500);
+//         }
+//         clearInterval(productWaitTimer);
+//     }
+// }, 25);
 
 
+//***************************//
+//***JQUERY IMPLEMENTATION***//
+//***************************//
 
+var $prodHeadElem = $('.backgroundfixed');
+var $prodElem = $('.productimages img');
+var $prodElemText = $('.productimages p');
+var $prodBoth = $prodElem.add($prodElemText);
+
+$prodHeadElem.css({'opacity': '0'}); //default to 0
+$prodHeadElem.animate({opacity: 1}, 1250, function(){
+    $prodBoth.css({
+        'margin-top': '-8%',
+        'top': '-8%',
+        'opacity': '0',
+    });
+
+    $prodBoth.animate({
+        'margin-top': '0%',
+        'top': '0%',
+    }, {duration: 1200, queue: false}, function(){
+    });
+
+    $prodBoth.animate({
+        'opacity': '1',
+    }, {duration: 1500, queue: false}, function(){
+    });
+
+});
